2fccafdcdf9651ee24f24eca67db9e464ea195fc
Faith Fechser <faithfechser@gmail.com>
2023-07-23 02:30:15 -0600

    add additional variables and the search event listener

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
+//Variables
 const apiKey = process.env.api_key;
+const searchForm = document.getElementById('search-form');
+const cityInput = document.getElementById('city-input');
+
+// Search form event listener
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const city = cityInput.value.trim();
+    if (city !== '') {
+        getWeatherData(city);
+        cityInput.value = '';
+    }
+});
