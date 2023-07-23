aebdefa7f9b7c55fcb6821eee04d67b78f93ea7c
Faith Fechser <faithfechser@gmail.com>
2023-07-23 14:01:17 -0600

    restart javascript code

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,4 @@
 //Variables
 const apiKey = process.env.api_key;
-const searchForm = document.getElementById('search-form');
+const searchBar = document.getElementById('search-bar');
 const cityInput = document.getElementById('city-input');
-
-// Search form event listener
-searchForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const city = cityInput.value.trim();
-    if (city !== '') {
-        getWeatherData(city);
-        cityInput.value = '';
-    }
-});
