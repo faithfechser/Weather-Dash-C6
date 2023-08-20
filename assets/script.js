e256dff23832647f3bfa77b4e13ddf65e8ebdebb
Faith Fechser <faithfechser@gmail.com>
2023-08-20 11:34:35 -0600

    add more connections and link to API, add API key

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,21 @@
 // Html connections
-const searchBar = document.getElementById("search-bar");
+const searchBar = document.getElementById("#search-bar");
+const userInput = document.getElementById("#city-input");
+const currentCity = document.querySelector(".currentCity");
+const currentDate = document.querySelector(".currentDate");
+const currentWeatherIcon = document.querySelector(".currentWeatherIcon");
+const currentTemp = document.getElementById("#currentTemp");
+const currentWind = document.getElementById("#currentWind");
+const currentHumidity = document.getElementById("#currentHumidity");
 
-// API key connection
-const apiKey = process.env.api_key;
+// API key
+const apiKey = 'f1b556da313e25b68a91bca1a8c4b597';
+// API connections
+const apiLat = `http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&appid=${apiKey}`;
+const apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${apiKey}`;
+
+async function fetchLat(){
+  const response = await fetch(apiLat)
+  const data = await response.json()
+  console.log(data)
+}
